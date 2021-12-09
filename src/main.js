3bd60314b6cf71ef116ac61f111926df3d7d54b9
Stella <stellamaia97@hotmail.com>
2021-12-09 15:47:58 -0300

    Alterações  JS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VMask from 'v-mask'
+
+
+import Vuelidate from 'vuelidate'
+
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -9,6 +14,9 @@ Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+
+Vue.use(Vuelidate)
+Vue.use(VMask);
 
 new Vue({
   render: h => h(App),
